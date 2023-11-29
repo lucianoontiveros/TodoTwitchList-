98b266965add3f0de31609816518c090c788bc02
luciano.a.ontiveros <luciano.a.ontiveros@gmail.com>
2023-11-28 21:51:08 -0300

    Se renueva el bot por completo

--- src/App.jsx
+++ src/App.jsx
@@ -83,7 +83,6 @@ function App() {
     }));
   };
 
-
   useEffect(() => {
     const client = new tmi.Client({
       options: { debug: false },
@@ -95,8 +94,6 @@ function App() {
     });
 
     client.connect(); 
-
-   
 
     const startInterval = () => {
       showTasksInterval = setInterval(() => {
@@ -234,7 +231,7 @@ function App() {
               usernamePerfil(infoUsaurio.user).tareas.map((i, index) => (
                 <li key={index}>
                   <a href="#" className={clases.style}>
-                    <span className="flex-1 ml-3 overflow-hidden">{i.tarea}</span>
+                    <span className="flex-1 text-2xl ml-3 overflow-hidden">{i.tarea}</span>
                     <span className="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-base font-medium text-black-500 bg-gray-700 rounded dark:bg-green-700 dark:text-green-400">{i.id}</span>
                   </a>
                 </li>
