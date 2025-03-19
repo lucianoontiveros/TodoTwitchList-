dae130ca8ee93fb84dede7ad841bb1af4c2ede18
Luciano Ontiveros <87582769+lucianoontiveros@users.noreply.github.com>
2025-03-19 14:02:50 -0300

    Update InfoUser_component.jsx
    Se genera prevalencia de la instancia de Mod como fist follow

--- src/components/InfoUser_component/InfoUser_component.jsx
+++ src/components/InfoUser_component/InfoUser_component.jsx
@@ -68,6 +68,9 @@ const InfoUser_component = ({ user, username }) => {
             case "camm_sss":
               newClasses.container = "camm_sss infoUSer_containers orange_fondo";
               break;
+              case "mont_opo":
+              newClasses.container = "mont_opo infoUSer_containers crema_fondo";
+              break;
           }
           break;
         case "prime":
