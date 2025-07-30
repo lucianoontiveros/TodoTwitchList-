3f04b542af0acfd2d1b1e17cab5401fb97c59973
lucianoontiveros <luciano.a.ontiveros@gmail.com>
2025-07-30 06:33:06 -0300

    ✨ refactor:  se agrega usaurio  flavia JEA

--- src/components/InfoUser_component/InfoUser_component.jsx
+++ src/components/InfoUser_component/InfoUser_component.jsx
@@ -111,9 +111,9 @@ const InfoUser_component = ({ user, username }) => {
               newClasses.container =
                 "sofiaantok infoUSer_containers amarrillo_fondo";
               break;
-              case "Flavia_2025_":
+              case "flavia_2025_":
                 newClasses.container =
-                  "Flavia_2025_ infoUSer_containers flavia_fondo";
+                  "flavia_2025_ infoUSer_containers flavia_fondo";
                 break;
           }
           break;
