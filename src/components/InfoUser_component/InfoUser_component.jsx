8c1d5c625cc6ca9d1a74396d25d9834c9b6a6d8c
lucianoontiveros <luciano.a.ontiveros@gmail.com>
2025-08-07 20:06:56 -0300

    ✨ refactor:  se agrega usaurio  mariong898

--- src/components/InfoUser_component/InfoUser_component.jsx
+++ src/components/InfoUser_component/InfoUser_component.jsx
@@ -119,9 +119,6 @@ const InfoUser_component = ({ user, username }) => {
             case "flavia_2025_":
               newClasses.container = "flavia_2025_ infoUSer_containers flavia_fondo";
               break;
-            case "mariong898":
-              newClasses.container = "mariong898 infoUSer_containers mostaza_fondo";
-              break;
           }
           break;
 
@@ -138,7 +135,7 @@ const InfoUser_component = ({ user, username }) => {
               newClasses.container = "mandaariina infoUSer_containers crema_fondo";
               break;
             case "cuartodechenz":
-              newClasses.container = "mariong898  infoUSer_containers mostaza_fondo";
+              newClasses.container = "mariong898 infoUSer_containers mostaza_fondo";
               break;
           }
           break;
