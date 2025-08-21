683c0adea2358c0778845bdbf1068ba2d6a7b5ce
lucianoontiveros <luciano.a.ontiveros@gmail.com>
2025-08-21 14:19:06 -0300

    ✨Se  subio el fondo especifico  para micaaavllr

--- src/assets/modBackgrounds.js
+++ src/assets/modBackgrounds.js
@@ -1,7 +1,6 @@
 
 import agos__________ from "./agos__________.jpg";
 import camm_sss from "./camm_sss.jpeg"; 
-import crissworkoutt from "./crissworkoutt.png";
 import mandaariina from "./mandaariina.jpg";
 import mont_opo from "./mont_opo.png";
 import mariong898 from "./mariong898.png";
