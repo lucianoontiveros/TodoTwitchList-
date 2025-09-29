044d2c4b02e4a539c1973f3417e8d62e59fa3a60
lucianoontiveros <luciano.a.ontiveros@gmail.com>
2025-09-29 06:12:04 -0300

    ✨Se sactualiza  fondo de  se actualiza flavia user

--- src/assets/vipBackgrounds.js
+++ src/assets/vipBackgrounds.js
@@ -1,5 +1,5 @@
 
-import flavia_2025_ from "./flavia_2025_.jpeg";
+import fla_vius_ from "./flavia_2025_.jpeg";
 import karlitarachel from "./karlitarachel.jpg";
 import mariong898 from "./mariong898.png";
 import sofamb1 from "./sofamb1.jpeg";
@@ -11,7 +11,7 @@ import summertime0805 from "./summertime0805.png";
 export const vipBackgrounds = {
   mariong898: mariong898,
   sofiaantok: sofiaantok,
-  flavia_2025_: flavia_2025_,
+  fla_vius_: fla_vius_,
   sofamb1: sofamb1,
   karlitarachel: karlitarachel,
   summertime0805 : summertime0805,
