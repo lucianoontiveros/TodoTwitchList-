28299102f5885417b5a24e050f32b66d2806a5cd
lucianoontiveros <luciano.a.ontiveros@gmail.com>
2025-10-05 16:34:47 -0300

    Se cargar imagen de cande_lp

--- src/assets/susBackgrounds.js
+++ src/assets/susBackgrounds.js
@@ -46,7 +46,7 @@ import butterflylander from "./butterflylander.jpeg";
 import camiichan15 from "./camiichan15.png"
 import lulaluzz from "./lulaluzz.jpeg"
 import andirivero1312 from "./andirivero1312.jpg"
-import cande_pl  from "./cande_pl.jpg"
+import cande_pl from "./cande_pl.jpg"
  
 export const susBackgrounds = {
     agos__________ : agos__________,
