4432149887bf579c110cd9afe7e85e1a4fdfc4a2
lucianoontiveros <luciano.a.ontiveros@gmail.com>
2025-10-05 16:28:33 -0300

    Se corrige error de del usuario de cande

--- src/assets/susBackgrounds.js
+++ src/assets/susBackgrounds.js
@@ -46,7 +46,7 @@ import butterflylander from "./butterflylander.jpeg";
 import camiichan15 from "./camiichan15.png"
 import lulaluzz from "./lulaluzz.jpeg"
 import andirivero1312 from "./andirivero1312.jpg"
-import cande_pl from "./cande_pl.jpg"
+import cande_pl  from "./cande_pl.jpg"
  
 export const susBackgrounds = {
     agos__________ : agos__________,
@@ -92,7 +92,6 @@ export const susBackgrounds = {
     camiichan15 : camiichan15,
     lulaluzz : lulaluzz,
     andirivero1312 : andirivero1312,
-
     sus_fondo : sus_fondo,
     vip_fondo : vip_fondo,
     mod_fondo : mod_fondo,
