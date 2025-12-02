4632232f3b887a7ec15ef293086b52c786560dca
lucianoontiveros <luciano.a.ontiveros@gmail.com>
2025-12-02 18:54:36 -0300

    🧉👑 se corrige omisión de fondo de luluprcl

--- src/assets/susBackgrounds.js
+++ src/assets/susBackgrounds.js
@@ -107,7 +107,7 @@ export const susBackgrounds = {
     diaaazm : diaaazm,
     boniataereunarata : boniataereunarata,
     hyant08: hyant08,
-    luluprcl: lulaluzz
+    luluprcl: luluprcl,
     
     sus_fondo : sus_fondo,
     vip_fondo : vip_fondo,
